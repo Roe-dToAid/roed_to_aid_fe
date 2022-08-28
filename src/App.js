5311002d5f55436ca2f4d67a3b387574b1cd457a
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-28 13:57:28 -0400

    Add useState and useEffect to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { Footer } from "./Components/Footer/Footer";
 import "./App.scss";
 
-function App() {
+// const App = () => {
+//   const [compliment, setCompliment] = useState("");
+//   const [advice, setAdvice] = useState("");
+//   const [favoriteCompliment, setFavoriteCompliment] = useLocalStorage(
+//     "complimentFavorite",
+//     []
+//   );
+//   const [favoriteAdvice, setFavoriteAdvice] = useLocalStorage(
+//     "adviceFavorite",
+//     []
+//   );
+//   const [error, setError] = useState();
+
+//   useEffect(() => {
+//     getNewCall("compliment");
+//     getNewCall("advice");
+//   }, []);
+const App = () => {
+  const [policies, setPolicies] = useState([]);
+  const [resources, setResources] = useState([]);
+  const [authorizedClinics, setAuthorizedClinics] = useState([]);
+  const [misinformationCenters, setMisinformationCenters] = useState([]);
+  const [error, setError] = useState();
+
   return (
     <main className="App">
       <NavBar />
       <Footer />
     </main>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+    <App />
+    {/* </React.StrictMode> */}
   </BrowserRouter>
 );
 
