91415bdbe10db0af7425544a6c1a9b2618cc92fc
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-28 13:36:53 -0400

    Add fetches for policies, authorized clinics, and resources

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavBar } from "./Components/NavBar/NavBar";
+import { Footer } from "./Components/Footer/Footer";
 import "./App.scss";
 
 function App() {
   return (
     <main className="App">
       <NavBar />
+      <Footer />
     </main>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+import logo from "./../../assets/Roe'dToAidLogo.png";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return <nav className="footer-container"></nav>;
+};
