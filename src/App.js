d8321dbd43f4046f5e4680baedf2f46c9625ad1b
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-28 17:11:05 -0400

    Move useEffect to clincs/states views

--- src/App.js
+++ src/App.js
@@ -2,29 +2,9 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
 import Routes from "./Components/Routes/Routes";
-import {
-  getAllStatePolicies,
-  getAllResources,
-  getAllMisinformationClinics,
-  getAllAuthorizedClinics,
-} from "./utils/apiCalls";
 import "./App.scss";
 
 const App = () => {
-  const [policies, setPolicies] = useState([]);
-  const [resources, setResources] = useState([]);
-  const [authorizedClinics, setAuthorizedClinics] = useState([]);
-  const [misinformationCenters, setMisinformationCenters] = useState([]);
-
-  useEffect(() => {
-    getAllStatePolicies().then((data) => setPolicies(data.data.state));
-    getAllResources().then((data) => setResources(data.data));
-    getAllAuthorizedClinics().then((data) => setAuthorizedClinics(data.data));
-    getAllMisinformationClinics().then((data) =>
-      setMisinformationCenters(data.data)
-    );
-  }, []);
-
   return (
     <main className="App">
       <NavBar />
