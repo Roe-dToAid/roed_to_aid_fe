7ef9541d250f8f77b7c388e0b6c41e99522a79e6
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-29 16:32:10 -0400

    Remove unused variables and imports

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Footer.scss";
 import logo from "./../../assets/Roe'dToAidLogo.png";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
