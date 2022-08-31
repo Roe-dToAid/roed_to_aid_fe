0c550e2a8cdb94645bbc5e8d730f56ba179f5457
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-31 18:58:32 -0400

    Add informationblock and preview block components and respective styling

---
+++ src/Components/InformationBlock/InformationBlock.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./InformationBlock.scss";
+
+const InformationBlock = () => {
+  return (
+    <div className="information-containers">
+      <div className="authorized-clinics-block">
+        <div className="authorized-header">
+          <h2>Authorized Clinics</h2>
+        </div>
+        <p>
+          Clinics that have licensed medical personnel and provide safe,
+          regulated abortion services. Providers will answer questions and
+          address concerns over the phone or in person. Materials and resources
+          are based on medical science and clearly state the kinds of services
+          provided.
+        </p>
+      </div>
+      <br />
+      <br />
+      <div className="misinformation-centers-block">
+        <div className="misinformation-header">
+          <h2>Misinformation Centers</h2>
+        </div>
+        <p>
+          Misinformation Centers or "Pregnancy Crisis Centers" are facilities
+          that often advertise as 'abortion clinics', however do not provide
+          abortion services. Many of these centers provide false, harmful
+          information and attempt to dissuade individuals from personal
+          decisions.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InformationBlock;
--- src/Views/HomeView/HomeView.js
+++ src/Views/HomeView/HomeView.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./HomeView.scss";
-import groupImage from "./../../assets/BackgroundRemoved.png";
+import groupImage from "./../../assets/DiverseGroup.png";
+
+import InformationBlock from "../../Components/InformationBlock/InformationBlock";
 const HomeView = () => {
   return (
-    <div className="welcome-container">
-      <img
-        src={groupImage}
-        alt="image-of-diverse-people"
-        className="people-img"
-      ></img>
-      <h1>A safe space to find a safe place.</h1>
+    <div className="home-container">
+      <div className="welcome-container-">
+        <img
+          src={groupImage}
+          alt="image-of-diverse-people"
+          className="group-image"
+        ></img>
+
+        <h2>A safe space to find a safe place.</h2>
+      </div>
+      <br />
+      <br />
+      <div className="information-block">
+        <InformationBlock />
+      </div>
     </div>
   );
 };
