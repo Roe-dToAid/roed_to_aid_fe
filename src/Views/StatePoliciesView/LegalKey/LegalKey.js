737f16169e30c5e5dba1f1d9c974c5bd0f63c167
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-08-31 15:49:15 -0600

    Add capital letters to key

--- src/Views/StatePoliciesView/LegalKey/LegalKey.js
+++ src/Views/StatePoliciesView/LegalKey/LegalKey.js
@@ -3,9 +3,9 @@ import React from 'react'
 const LegalKey = () => {
   return (
     <div className='legal-key'>
-      <h2 className='legal'>Legal</h2>
-      <h2 className='at-risk'>At Risk</h2>
-      <h2 className='illegal'>Illegal</h2>
+      <h2 className='legal'>LEGAL</h2>
+      <h2 className='at-risk'>AT RISK</h2>
+      <h2 className='illegal'>ILLEGAL</h2>
     </div>
   )
 }
