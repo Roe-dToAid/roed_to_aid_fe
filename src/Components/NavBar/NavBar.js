bf4fc0530c69ed222d197f96b9484f4f054db054
Rachel Allen <rallenco13@gmail.com>
2022-08-31 18:22:28 -0600

    added in logo and hero image, cleaning up nav styling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./NavBar.scss";
-import logo from "./../../assets/Roe'dToAidLogo.png";
+import logo from "./../../assets/roed-to-aid-logo.svg";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="nav-container">
-        <Link to="/" className="logo-link">
-          <div className="logo-container">
-            <img src={logo} alt="logo" className="logo-img"></img>
-          </div>
+      <Link to="/" className="logo-link">
+        <div className="logo-container">
+          <img src={logo} alt="logo" className="logo-img"></img>
+        </div>
+      </Link>
+      <div className="nav-tabs-container">
+        <Link to="/about" className="nav-tabs">
+          <p>ABOUT</p>
         </Link>
-            <div className="nav-tabs">
-              <Link to="/about" className="nav-tabs">
-                <h1>ABOUT</h1>
-              </Link>
-              <Link to="/clinics" className="nav-tabs">
-                <h1>FIND A CLINIC</h1>
-              </Link>
-              <Link to='/states-policies' className="nav-tabs">
-                <h1>STATE STATUS</h1>
-              </Link>
-          </div>
+        <Link to="/clinics" className="nav-tabs">
+          <p>FIND A CLINIC</p>
+        </Link>
+        <Link to="/states-policies" className="nav-tabs">
+          <p>STATE STATUS</p>
+        </Link>
+      </div>
     </nav>
   );
 };
