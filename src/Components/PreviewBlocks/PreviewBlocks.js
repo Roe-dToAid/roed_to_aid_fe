8c813cf15c1fc8405bdef7b103bca24557545987
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-31 22:01:46 -0400

    Add icons to previewblocks and label edits

--- src/Components/PreviewBlocks/PreviewBlocks.js
+++ src/Components/PreviewBlocks/PreviewBlocks.js
@@ -1,30 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import clinic_img from "./../../assets/Asset 26.svg";
+import gavel_img from "./../../assets/Asset 31.svg";
+import speaker_img from "./../../assets/Asset 28.svg";
+
 import "./PreviewBlocks.scss";
 
 const PreviewBlocks = () => {
   return (
     <>
       <div className="preview">
+        <img src={clinic_img} alt="image-of-uterus" className="icon"></img>
         <p>
-          Find a safe place to receive an abortion and avoid these
-          misinformation centers. Learn what resources are available in states
-          with total bans.
+          Find a safe place to receive an abortion and avoid misinformation
+          centers. Learn what resources are available in states with total bans.
         </p>
         <Link to="/clinics">
           <button>Find A Clinic</button>
         </Link>
       </div>
       <div className="preview">
+        <img src={gavel_img} alt="image-of-gavel" className="icon"></img>
+
         <p>
-          State laws are changing now that Roe V Wade has been struck down. Find
-          your state to learn more.
+          After Roe v. Wade was overturned on June 24, 2022, abortion policy is
+          in the hands of the states. Find your state to learn more.
         </p>
         <Link to="/states-policies">
           <button>Check State Status</button>
         </Link>
       </div>
       <div className="preview">
+        <img src={speaker_img} alt="image-of-speaker" className="icon"></img>
         <p>
           We are driven by the cause. We believe in inclusivity, access, and
           education to keep birthers safe.
