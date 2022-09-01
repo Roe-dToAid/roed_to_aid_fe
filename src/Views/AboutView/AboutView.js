756a35479799428eb0f0a958e4053318e230c439
Irmak Erdem <irmakerdem9@gmail.com>
2022-09-01 11:54:39 -0600

    Add styling to about page

--- src/Views/AboutView/AboutView.js
+++ src/Views/AboutView/AboutView.js
@@ -18,25 +18,28 @@ const AboutView = () => {
       <div className='mission-container'>
         <h2>Our Mission</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img src={yesImage} alt="person holding up fist"></img>
       </div>
+      
+      <img className='illustration' src={yesImage} alt="person holding up fist"></img>
 
-      <div className='inclusivity container'>
+      <div className='inclusivity-container'>
         <h3>Inclusivity Counts</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img src={flowerSymbolImage} alt="female symbol with flowers"></img>
       </div>
+      
+      <img className='illustration' src={flowerSymbolImage} alt="female symbol with flowers"></img>
       
       <div className='team-container'>
         <h4>Meet the Team</h4>
+        <img src={rachelPhoto} alt="headshot of Rachel"></img>
         <img src={catlynPhoto} alt="headshot of Catlyn"></img>
         <img src={irmakPhoto} alt="headshot of Irmak"></img>
         <img src={lidiaPhoto} alt="headshot of Lidia"></img>
-        <img src={rachelPhoto} alt="headshot of Rachel"></img>
-        <img src={brycePhoto} alt="headshot of Bryce"></img>
         <img src={caseyPhoto} alt="headshot of Casey"></img>
         <img src={wesPhoto} alt="headshot of Wes"></img>
+        <img src={brycePhoto} alt="headshot of Bryce"></img>
       </div>
+
     </div>
   )
 };
