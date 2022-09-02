84bb49d0b7765d20bf3b820e4e282d6c133dd2c2
Irmak Erdem <irmakerdem9@gmail.com>
2022-09-02 14:50:07 -0600

    Add testing for inclusivity section

--- cypress/e2e/aboutview.cy.js
+++ cypress/e2e/aboutview.cy.js
@@ -3,10 +3,17 @@ describe('About', () => {
     cy.visit('http://localhost:3000/about');
   })
 
-  it('The user should see the About page', () => {
+  it('Should display the About page and the mission', () => {
     cy.dataCy("about-roed").contains("About Roe'd to Aid")
     cy.dataCy("mission-title").contains("Our Mission")
     cy.dataCy("mission-statement").contains("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    cy.dataCy("yes-illustration").should("be.visible")
+  })
+
+  it('Should display the inclusivity message', () => {
+    cy.dataCy("inclusivity-title").contains("Inclusivity Counts")
+    cy.dataCy("inclusivity-statement").contains("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    cy.dataCy("flower-illustration").should("be.visible")
   })
 
   it('The user should be able to go back to the Home page when clicking logo', () => {
--- src/Views/AboutView/AboutView.js
+++ src/Views/AboutView/AboutView.js
@@ -23,8 +23,8 @@ const AboutView = () => {
       <img className='illustration' data-cy='yes-illustration' src={yesImage} alt="person holding up fist"></img>
 
       <div className='inclusivity-container'>
-        <h3>Inclusivity Counts</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h3 data-cy='inclusivity-title'>Inclusivity Counts</h3>
+        <p data-cy='inclusivity-statement'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
       
       <img className='illustration' data-cy='flower-illustration' src={flowerSymbolImage} alt="female symbol with flowers"></img>
