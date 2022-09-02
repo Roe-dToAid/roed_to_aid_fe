bb7e78fc6092d02d409a366a75a9fe350c2c51c0
Rachel Allen <rallenco13@gmail.com>
2022-09-02 17:59:45 -0600

    got all clinics rendering and applied some styling

--- src/Components/ClinicCard/ClinicCard.js
+++ src/Components/ClinicCard/ClinicCard.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./ClinicCard.scss";
 
-const ClinicCard = ({ name, address }) => {
+const ClinicCard = ({ name, address, city, zip, phone, services, url }) => {
   return (
     <div className="clinic-card" data-cy="clinic-card">
-      <h2 data-cy="misinformation-clinic-name">
-        {name}
-      </h2>
-      <h2 data-cy="misinformation-clinic-address">
-        {address}
-      </h2>
+      <h3 data-cy="clinic-name">{name}</h3>
+      <p data-cy="clinic-address">{address}</p>
+      <p data-cy="clinic-city">{city}</p>
+      <p data-cy="clinic-zip">{zip}</p>
+      <p data-cy="clinic-phone">{phone}</p>
+      <p data-cy="clinic-services">{services}</p>
+      <a href={url} target="blank" data-cy="clinic-url">
+        Visit their website
+        <span className="material-symbols-outlined">open_in_new</span>
+      </a>
     </div>
   );
 };
--- src/Components/ClinicCardContainer/ClinicCardContainer.js
+++ src/Components/ClinicCardContainer/ClinicCardContainer.js
@@ -11,12 +11,34 @@ const ClinicCardContainer = ({ states }) => {
           key={clinic.id}
           name={clinic.name}
           address={clinic.address}
+          city={clinic.city}
+        />
+      );
+    });
+  });
+  const authorizedClinics = states.map((state) => {
+    return state.authorizedClinics.map((clinic) => {
+      return (
+        <ClinicCard
+          key={clinic.id}
+          name={clinic.name}
+          address={clinic.address}
+          city={clinic.city}
+          zip={clinic.zip}
+          phone={clinic.phone}
+          services={clinic.services}
+          url={clinic.website}
         />
       );
     });
   });
   return (
-    <section className="clinic-card-container">{misinformationCenters}</section>
+    <section className="all-clinics-card-container">
+      <div className="authorized-card-container">{authorizedClinics}</div>
+      <div className="misinformation-card-container">
+        {misinformationCenters}
+      </div>
+    </section>
   );
 };
 
