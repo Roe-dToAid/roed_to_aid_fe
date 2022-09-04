554824afcb32f8e76a82833ce1284b5edc31de70
Irmak Erdem <irmakerdem9@gmail.com>
2022-09-04 15:24:25 -0600

    Create Loading component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import "./App.scss";
 const App = () => {
   return (
     <main className="App">
+      {/* if (loading) {
+          return <h1>LOADING...</h1>
+        }
+
+        if (error) {
+          console.log(error)
+          return <h1>SOMETHING WENT WRONG...</h1>
+        } */}
       <NavBar />
       <Routes />
       <Footer />
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loading.scss';
+
+const Loading = () => {
+	return (
+    <h1>⏳ LOADING... ⏳</h1>
+	);
+};
+
+export default Loading;
