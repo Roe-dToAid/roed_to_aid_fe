7a2b5738b26b94a409ad18fc315ec79b5cf14e5c
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-09-05 21:07:36 -0600

    Add successful tests for filter methods"

--- cypress/e2e/statepolicies.cy.js
+++ cypress/e2e/statepolicies.cy.js
@@ -3,56 +3,82 @@
 //     cy.visit('http://localhost:3000/states-policies')
 //   });
 
-//   it('Should have a page title', () => {
-//     cy.dataCy('policy-header').contains('Check state abortion status')
-//   })
+import policies from '../fixtures/policies.json';
 
-//   it('Should have a search bar', () => {
-//     cy.dataCy('search').should('exist')
-//   })
+describe('State Policies View', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/states-policies');
+    cy.interceptGQL(
+      'https://roed-to-aide-be.herokuapp.com/graphql?api_key=ca912ed1df0d1c0f014ec94e3c731881',
+      'GetPolicies',
+      policies
+    ).as('GetPolicies');
+    cy.wait("@GetPolicies")
+  });
 
-//   it('Should have a legend to search by legality, including a clear button', () => {
-//     cy.dataCy('legal-key').should('exist')
+  it('Should have a page title', () => {
+    cy.dataCy('policy-header').contains('Check state abortion status')
+  })
 
-//     cy.dataCy('legal-key').children().should('have.length', 4)
-//     cy.dataCy('legal-key').children().first().contains('Clear Search')
-//     cy.dataCy('legal-key').children().next().contains('LEGAL')
-//     cy.dataCy('legal-key').children().next().contains('AT RISK')
-//     cy.dataCy('legal-key').children().next().contains('ILLEGAL')
-//   })
+  it('Should have a search bar', () => {
+    cy.dataCy('search').should('exist')
+  })
 
-//   it('Should have icons', () => {
-//     cy.dataCy('fight-img').should('exist')
-//     cy.dataCy('fists-img').should('exist')
-//   })
+  it('Should have a legend to search by legality, including an all states button', () => {
+    cy.dataCy('legal-key').should('exist')
 
-//   it('Should upload all policy cards on page load', () => {
-//     cy.dataCy('info-strip').should('have.length', 52)
-//   })
+    cy.dataCy('legal-key').children().should('have.length', 4)
+    cy.dataCy('legal-key').children().first().contains('ALL STATES')
+    cy.dataCy('legal-key').children().next().contains('LEGAL')
+    cy.dataCy('legal-key').children().next().contains('AT RISK')
+    cy.dataCy('legal-key').children().next().contains('ILLEGAL')
+  })
 
-//   it('Should display policys cards in alphabetical order with the state name and legal description', () => {
-//     cy.dataCy('info-strip').children().first().contains('Alabama')
-//     cy.dataCy('info-strip').children().next().contains('Abortion is completely banned in Alabama')
+  it('Should have icons', () => {
+    cy.dataCy('fight-img').should('exist')
+    cy.dataCy('fists-img').should('exist')
+  })
 
-//     cy.dataCy('info-strip').children().next().contains('Alaska')
-//     cy.dataCy('info-strip').children().next().contains('Yes. Abortion is legal in Alaska. There is no limit on abortion in Alaska based on how far along in pregnancy you are. To figure out how far along you are in pregnancy, count from the first day of your last period.')
-//   })
+  it.skip('Should upload all policy cards on page load', () => {
+    cy.interceptGQL(
+      'https://roed-to-aide-be.herokuapp.com/graphql?api_key=ca912ed1df0d1c0f014ec94e3c731881',
+      'GetPolicies',
+      policies
+    ).as('GetPolicies');
+    
+    cy.dataCy('info-strip').should('have.length', 3)
+  })
 
-  //NOT WORKING
-  // it('Should should allow the user to sort by legal status', () => {
-  //   cy.dataCy('legal').click({timeout: 2000}).get('info-strip').should('have.length', 28)
-  // })
+  it('Should display policys cards in alphabetical order with the state name and legal description', () => {
+    cy.dataCy('info-strip').children().first().contains('Alabama')
+    cy.dataCy('info-strip').children().next().contains('Abortion is completely banned in Alabama')
 
-  // it('Should should allow the user to sort by legal status', () => {
-  //   cy.contains('ILLEGAL').click().first().dataCy('info-strip').contains('Alabama')
-  //  })
+    cy.dataCy('info-strip').children().next().contains('Alaska')
+    cy.dataCy('info-strip').children().next().contains('Yes. Abortion is legal in Alaska. There is no limit on abortion in Alaska based on how far along in pregnancy you are. To figure out how far along you are in pregnancy, count from the first day of your last period.')
+  })
 
-  //  it('Should should allow the user to sort by legal status', () => {
-  //   cy.contains('AT RISK').click().first().dataCy('info-strip').contains('Arizona')
-  //  })
+  // NOT WORKING
+  it('Should should allow the user to sort by legal status', () => {
+    cy.dataCy('legal').click({ timeout: 10000 })
+    cy.wait(2000)
+    cy.dataCy('info-strip').should('have.length', 28)
+  })
 
-  //  it('Should should allow the user to search with the search bar', () => {
-  //   cy.get('.search').type('Alaska')
-  //   // cy.dataCy('info-strip').should('have.length', 1)
-  //  })
-// })
+  it('Should should allow the user to sort by legal status', () => {
+    cy.dataCy('illegal').click({ timeout: 10000 })
+    cy.wait(2000)
+    cy.dataCy('info-strip').should('have.length', 14)
+   })
+
+   it('Should should allow the user to sort by legal status', () => {
+    cy.dataCy('at-risk').click({ timeout: 10000 })
+    cy.wait(2000)
+    cy.dataCy('info-strip').should('have.length', 10)
+   })
+
+   it('Should should allow the user to search with the search bar', () => {
+    cy.get('.search').type('Alaska')
+    cy.dataCy('info-strip').should('have.length', 1)
+    cy.get('.search').clear()
+   })
+})
--- src/Components/LegalKey/LegalKey.js
+++ src/Components/LegalKey/LegalKey.js
@@ -5,8 +5,8 @@ const LegalKey = ({filterByLegality, clearSearch}) => {
     <div className='legal-key' data-cy='legal-key'>
       <h2 className='clear' role='button' tabIndex='0' onClick={() => clearSearch()}>ALL STATES</h2>
       <h2 className='legal' data-cy='legal' role='button' tabIndex='0' onClick={() => filterByLegality('legal')}>LEGAL</h2>
-      <h2 className='at-risk' role='button' tabIndex='0' onClick={() => filterByLegality('at risk')}>AT RISK</h2>
-      <h2 className='illegal' role='button' tabIndex='0' onClick={() => filterByLegality('illegal')}>ILLEGAL</h2>
+      <h2 className='at-risk' data-cy='at-risk' role='button' tabIndex='0' onClick={() => filterByLegality('at risk')}>AT RISK</h2>
+      <h2 className='illegal' data-cy='illegal' role='button' tabIndex='0' onClick={() => filterByLegality('illegal')}>ILLEGAL</h2>
     </div>
   )
 }
