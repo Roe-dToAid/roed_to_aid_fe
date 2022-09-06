34fe4736eb1f58abaa4ee7fa48032e4cc0e12ade
Irmak Erdem <irmakerdem9@gmail.com>
2022-09-06 12:03:16 -0600

    Add tab indeces to about page

--- src/Views/AboutView/AboutView.js
+++ src/Views/AboutView/AboutView.js
@@ -13,50 +13,50 @@ import "./Aboutview.scss";
 const AboutView = () => {
   return (
     <div className="about-container">
-      <h1 data-cy="about-roed">About Roe'd to Aid</h1>
+      <h1 tabindex="0" data-cy="about-roed">About Roe'd to Aid</h1>
       <div className="copy-image-block">
         <div className="mission-container">
-          <h2 data-cy="mission-title">Our Mission</h2>
-          <p data-cy="mission-statement">Our mission is to make abortion clinics information accessible to everyone in the US.</p>
+          <h2 tabindex="0" data-cy="mission-title">Our Mission</h2>
+          <p tabindex="0" data-cy="mission-statement">Our mission is to make abortion clinics information accessible to everyone in the US.</p>
         </div>
-        <img className="illustration" data-cy="yes-illustration" src={yesImage} alt="person holding up fist"></img>
+        <img tabindex="0" className="illustration" data-cy="yes-illustration" src={yesImage} alt="person holding up fist"></img>
       </div>
       <div className="copy-image-block inclusivity-only">
-        <img className="illustration" data-cy="flower-illustration" src={flowerSymbolImage} alt="female symbol with flowers"></img>
+        <img tabindex="0" className="illustration" data-cy="flower-illustration" src={flowerSymbolImage} alt="female symbol with flowers"></img>
         <div className="inclusivity-container">
-          <h2 data-cy="inclusivity-title">Inclusivity Counts</h2>
-          <p data-cy="inclusivity-statement">Abortion affects everyone. Regardless of your income, gender, sexual orientation, race, and other characteristics, we all must stay informed about current laws. Abortion is essential.</p>
+          <h2 tabindex="0" data-cy="inclusivity-title">Inclusivity Counts</h2>
+          <p tabindex="0" data-cy="inclusivity-statement">Abortion affects everyone. Regardless of your income, gender, sexual orientation, race, and other characteristics, we all must stay informed about current laws. Abortion is essential.</p>
         </div>
       </div>
-      <h2 data-cy="meet">Meet the Team</h2>
+      <h2 tabindex="0" data-cy="meet">Meet the Team</h2>
       <div className="team-container">
         <div className="photo-container">
-          <img data-cy="rachel" src={rachelPhoto} alt="headshot of Rachel"></img>
-          <h3>Rachel A.</h3>
+          <img tabindex="0" data-cy="rachel" src={rachelPhoto} alt="headshot of Rachel"></img>
+          <h3 tabindex="0">Rachel A.</h3>
         </div>
         <div className="photo-container">
-          <img data-cy="catlyn" src={catlynPhoto} alt="headshot of Catlyn"></img>
-          <h3>Catlyn B.</h3>
+          <img tabindex="0" data-cy="catlyn" src={catlynPhoto} alt="headshot of Catlyn"></img>
+          <h3 tabindex="0">Catlyn B.</h3>
         </div>
         <div className="photo-container">
-          <img data-cy="irmak" src={irmakPhoto} alt="headshot of Irmak"></img>
-          <h3>Irmak E.</h3>
+          <img tabindex="0" data-cy="irmak" src={irmakPhoto} alt="headshot of Irmak"></img>
+          <h3 tabindex="0">Irmak E.</h3>
         </div>
         <div className="photo-container">
-          <img data-cy="lidia" src={lidiaPhoto} alt="headshot of Lidia"></img>
-          <h3>Lidia K.</h3>
+          <img tabindex="0" data-cy="lidia" src={lidiaPhoto} alt="headshot of Lidia"></img>
+          <h3 tabindex="0">Lidia K.</h3>
         </div>
         <div className="photo-container">
-          <img data-cy="casey" src={caseyPhoto} alt="headshot of Casey"></img>
-          <h3>Casey F.</h3>
+          <img tabindex="0"data-cy="casey" src={caseyPhoto} alt="headshot of Casey"></img>
+          <h3 tabindex="0">Casey F.</h3>
         </div>
         <div className="photo-container">
-          <img data-cy="wes" src={wesPhoto} alt="headshot of Wes"></img>
-          <h3>Wes G.</h3>
+          <img tabindex="0" data-cy="wes" src={wesPhoto} alt="headshot of Wes"></img>
+          <h3 tabindex="0">Wes G.</h3>
         </div>
         <div className="photo-container">
-          <img data-cy="bryce" src={brycePhoto} alt="headshot of Bryce"></img>
-          <h3>Bryce W.</h3>
+          <img tabindex="0" data-cy="bryce" src={brycePhoto} alt="headshot of Bryce"></img>
+          <h3 tabindex="0">Bryce W.</h3>
         </div>
       </div>
     </div>
