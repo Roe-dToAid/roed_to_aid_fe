ce061d8fca337df070228b51ab057d0cc4926e21
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-09-07 11:58:39 -0400

    Resolve merge conflicts in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,11 @@
-import React from 'react';
-import './Footer.scss';
-import logo from './../../assets/roed-to-aid-logo.svg';
+import React from "react";
+import "./Footer.scss";
+import logo from "./../../assets/roed-to-aid-logo.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
-<<<<<<< HEAD
-      <img
-        src={logo}
-        alt="logo"
-        className="logo-img"
-        data-cy="logo-img"
-        tabIndex={0}
-      ></img>
-=======
       <img src={logo} alt="logo" className="logo-img" data-cy="logo-img"></img>
->>>>>>> main
       <div className="footer-team-container">
         <div className="fe-container">
           <p data-cy="fe-team" tabIndex={0}>
@@ -27,16 +17,12 @@ const Footer = () => {
             data-cy="git-hub-link-rachel"
           >
             Rachel Allen
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
           <a
             href="https://github.com/catlynbowles"
@@ -44,16 +30,12 @@ const Footer = () => {
             data-cy="git-hub-link-catlyn"
           >
             Catlyn Bowles
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
           <a
             href="https://github.com/irmakerdem"
@@ -61,16 +43,12 @@ const Footer = () => {
             data-cy="git-hub-link-irmak"
           >
             Irmak Erdem
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
           <a
             href="https://github.com/lkarpins"
@@ -78,16 +56,12 @@ const Footer = () => {
             data-cy="git-hub-link-lidia"
           >
             Lidia Karpinski
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
         </div>
         <div className="be-container">
@@ -100,16 +74,12 @@ const Footer = () => {
             data-cy="git-hub-link-casey"
           >
             Casey Fazio
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
           <a
             href="https://github.com/wesatt"
@@ -117,16 +87,12 @@ const Footer = () => {
             data-cy="git-hub-link-wesley"
           >
             Wesley Garcia
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
           <a
             href="https://github.com/bwbolt"
@@ -134,16 +100,12 @@ const Footer = () => {
             data-cy="git-hub-link-bryce"
           >
             Bryce Wein
-<<<<<<< HEAD
-            <span className="material-symbols-outlined">open_in_new</span>
-=======
             <span
               className="material-symbols-outlined"
               aria-label="git-hub. open in new tab"
             >
               open_in_new
             </span>
->>>>>>> main
           </a>
         </div>
       </div>
