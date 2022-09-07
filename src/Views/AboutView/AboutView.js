236578259581308a4c0ad7e826e5ef93e0cfc0c3
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-09-07 12:30:14 -0400

    Add mission statement and data container styling

--- src/Views/AboutView/AboutView.js
+++ src/Views/AboutView/AboutView.js
@@ -22,7 +22,13 @@ const AboutView = () => {
             <h2 data-cy="mission-title">Our Mission</h2>
             <p data-cy="mission-statement">
               Our mission is to make abortion clinics information accessible to
-              everyone in the US.
+              everyone in the US. The reversal of Roe V. Wade will not reduce
+              abortions; it will limit access to safe abortions and endanger
+              millions of birthers. Roe'd To Aid was created to empower folks by
+              educating on current state policies, exposing misinformation
+              centers, and promoting authorized abortion clinics. Users gain the
+              power to make informed decisions about where they may go to
+              receive necessary healthcare.
             </p>
           </div>
           <img
@@ -88,10 +94,23 @@ const AboutView = () => {
           </div>
         </div>
         <div data-cy="data-statement" className="data-statement">
-          <p>The data on this website was last updated on September 6, 2022.</p>
           <p>
-            For most up to date information, please go{" "}
-            <a href="https://www.abortionfinder.org">here</a>.
+            <strong>
+              The data on this website was last updated on September 6, 2022.
+            </strong>
+            <br />
+            Roe'd To Aid attempts to provide access to information in one place
+            by combining resources found from many wonderful sites already in
+            existence.
+          </p>
+          <p>
+            For abortion seekers hoping to find the latest information we
+            recommend checking out these links below, as well as any of the
+            sites listed in the resources section.
+            <br />
+            <a href="https://www.abortionfinder.org">Abortion Finder</a>.
+            <br />
+            <a href="https://www.ineedana.com">I need an A</a>.
           </p>
         </div>
       </div>
